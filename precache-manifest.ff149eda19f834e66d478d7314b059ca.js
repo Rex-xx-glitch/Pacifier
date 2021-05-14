b755f4bebdff8f311402125420d4d3d9e6144810
Rex-xx-glitch <gumboraymacantony@gmail.com>
2021-05-14 10:26:53 +0300

    Updates

---
+++ precache-manifest.ff149eda19f834e66d478d7314b059ca.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4773e8416521220224635afe45fc9bfb",
+    "url": "/pacifer/index.html"
+  },
+  {
+    "revision": "fa8e2669cf6584133813",
+    "url": "/pacifer/static/css/main.41d49b5e.chunk.css"
+  },
+  {
+    "revision": "3a0b5cd4ef9e0c23b430",
+    "url": "/pacifer/static/js/2.746fc4b2.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/pacifer/static/js/2.746fc4b2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fa8e2669cf6584133813",
+    "url": "/pacifer/static/js/main.1b40ff4f.chunk.js"
+  },
+  {
+    "revision": "605fd4f15fcf5016dbfa",
+    "url": "/pacifer/static/js/runtime-main.89398a7e.js"
+  }
+]);
